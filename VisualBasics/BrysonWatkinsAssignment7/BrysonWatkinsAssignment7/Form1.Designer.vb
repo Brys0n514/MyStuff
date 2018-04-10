00f283c7839ca9774c93b8e4ec461bed632f9cd8
Bryson Watkins <31283951+Brys0n514@users.noreply.github.com>
2018-04-10 12:14:08 -0500

    Add files via upload

---
+++ VisualBasics/BrysonWatkinsAssignment7/BrysonWatkinsAssignment7/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintSummaryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintPreviewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintYachtTypesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintPreviewToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripTextBox1 = New System.Windows.Forms.ToolStripTextBox()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintPreviewSummary = New System.Windows.Forms.PrintPreviewDialog()
+        Me.PrintYachtsControl = New System.Drawing.Printing.PrintDocument()
+        Me.PrintSummaryControl = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewYacht = New System.Windows.Forms.PrintPreviewDialog()
+        Me.YachtsComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.CalcButton = New System.Windows.Forms.Button()
+        Me.yachtLabel = New System.Windows.Forms.Label()
+        Me.yachtSizeListBox1 = New System.Windows.Forms.ListBox()
+        Me.YachtSizeLabel1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.inputHoursLabel1 = New System.Windows.Forms.Label()
+        Me.HoursTextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(462, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintSummaryToolStripMenuItem, Me.PrintYachtTypesToolStripMenuItem, Me.ExitToolStripMenuItem, Me.ToolStripTextBox1})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'PrintSummaryToolStripMenuItem
+        '
+        Me.PrintSummaryToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintPreviewToolStripMenuItem, Me.PrintToolStripMenuItem})
+        Me.PrintSummaryToolStripMenuItem.Name = "PrintSummaryToolStripMenuItem"
+        Me.PrintSummaryToolStripMenuItem.Size = New System.Drawing.Size(166, 22)
+        Me.PrintSummaryToolStripMenuItem.Text = "Print &Summary"
+        '
+        'PrintPreviewToolStripMenuItem
+        '
+        Me.PrintPreviewToolStripMenuItem.Name = "PrintPreviewToolStripMenuItem"
+        Me.PrintPreviewToolStripMenuItem.Size = New System.Drawing.Size(143, 22)
+        Me.PrintPreviewToolStripMenuItem.Text = "Print P&review"
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(143, 22)
+        Me.PrintToolStripMenuItem.Text = "&Print"
+        '
+        'PrintYachtTypesToolStripMenuItem
+        '
+        Me.PrintYachtTypesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintPreviewToolStripMenuItem1, Me.PrintToolStripMenuItem1})
+        Me.PrintYachtTypesToolStripMenuItem.Name = "PrintYachtTypesToolStripMenuItem"
+        Me.PrintYachtTypesToolStripMenuItem.Size = New System.Drawing.Size(166, 22)
+        Me.PrintYachtTypesToolStripMenuItem.Text = "Print &Yacht Types"
+        '
+        'PrintPreviewToolStripMenuItem1
+        '
+        Me.PrintPreviewToolStripMenuItem1.Name = "PrintPreviewToolStripMenuItem1"
+        Me.PrintPreviewToolStripMenuItem1.Size = New System.Drawing.Size(143, 22)
+        Me.PrintPreviewToolStripMenuItem1.Text = "Print P&review"
+        '
+        'PrintToolStripMenuItem1
+        '
+        Me.PrintToolStripMenuItem1.Name = "PrintToolStripMenuItem1"
+        Me.PrintToolStripMenuItem1.Size = New System.Drawing.Size(143, 22)
+        Me.PrintToolStripMenuItem1.Text = "&Print"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(163, 6)
+        '
+        'ToolStripTextBox1
+        '
+        Me.ToolStripTextBox1.Name = "ToolStripTextBox1"
+        Me.ToolStripTextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.ToolStripTextBox1.Text = "&Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'PrintPreviewSummary
+        '
+        Me.PrintPreviewSummary.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewSummary.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewSummary.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewSummary.Enabled = True
+        Me.PrintPreviewSummary.Icon = CType(resources.GetObject("PrintPreviewSummary.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewSummary.Name = "PrintPreviewSummary"
+        Me.PrintPreviewSummary.Visible = False
+        '
+        'PrintSummaryControl
+        '
+        '
+        'PrintPreviewYacht
+        '
+        Me.PrintPreviewYacht.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewYacht.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewYacht.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewYacht.Enabled = True
+        Me.PrintPreviewYacht.Icon = CType(resources.GetObject("PrintPreviewYacht.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewYacht.Name = "PrintPreviewYacht"
+        Me.PrintPreviewYacht.Visible = False
+        '
+        'YachtsComboBox1
+        '
+        Me.YachtsComboBox1.FormattingEnabled = True
+        Me.YachtsComboBox1.Items.AddRange(New Object() {"Ranger", "Wavelength", "Catalina ", "Coronado ", "Hobie", "C & C", "Hans Chritian", "Excalibur"})
+        Me.YachtsComboBox1.Location = New System.Drawing.Point(53, 69)
+        Me.YachtsComboBox1.Name = "YachtsComboBox1"
+        Me.YachtsComboBox1.Size = New System.Drawing.Size(163, 21)
+        Me.YachtsComboBox1.TabIndex = 2
+        '
+        'CalcButton
+        '
+        Me.CalcButton.Location = New System.Drawing.Point(53, 268)
+        Me.CalcButton.Name = "CalcButton"
+        Me.CalcButton.Size = New System.Drawing.Size(75, 23)
+        Me.CalcButton.TabIndex = 3
+        Me.CalcButton.Text = "&Ok"
+        Me.CalcButton.UseVisualStyleBackColor = True
+        '
+        'yachtLabel
+        '
+        Me.yachtLabel.AutoSize = True
+        Me.yachtLabel.Location = New System.Drawing.Point(50, 53)
+        Me.yachtLabel.Name = "yachtLabel"
+        Me.yachtLabel.Size = New System.Drawing.Size(65, 13)
+        Me.yachtLabel.TabIndex = 4
+        Me.yachtLabel.Text = "Yacht Type:"
+        '
+        'yachtSizeListBox1
+        '
+        Me.yachtSizeListBox1.FormattingEnabled = True
+        Me.yachtSizeListBox1.Items.AddRange(New Object() {"22", "24", "30", "36", "38", "45"})
+        Me.yachtSizeListBox1.Location = New System.Drawing.Point(53, 157)
+        Me.yachtSizeListBox1.Name = "yachtSizeListBox1"
+        Me.yachtSizeListBox1.Size = New System.Drawing.Size(78, 82)
+        Me.yachtSizeListBox1.TabIndex = 5
+        '
+        'YachtSizeLabel1
+        '
+        Me.YachtSizeLabel1.AutoSize = True
+        Me.YachtSizeLabel1.Location = New System.Drawing.Point(50, 141)
+        Me.YachtSizeLabel1.Name = "YachtSizeLabel1"
+        Me.YachtSizeLabel1.Size = New System.Drawing.Size(58, 13)
+        Me.YachtSizeLabel1.TabIndex = 6
+        Me.YachtSizeLabel1.Text = "Yacht Size"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(53, 96)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Add Yacht"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'inputHoursLabel1
+        '
+        Me.inputHoursLabel1.AutoSize = True
+        Me.inputHoursLabel1.Location = New System.Drawing.Point(285, 53)
+        Me.inputHoursLabel1.Name = "inputHoursLabel1"
+        Me.inputHoursLabel1.Size = New System.Drawing.Size(35, 13)
+        Me.inputHoursLabel1.TabIndex = 8
+        Me.inputHoursLabel1.Text = "Hours"
+        '
+        'HoursTextBox1
+        '
+        Me.HoursTextBox1.Location = New System.Drawing.Point(288, 70)
+        Me.HoursTextBox1.Name = "HoursTextBox1"
+        Me.HoursTextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.HoursTextBox1.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 381)
+        Me.Controls.Add(Me.HoursTextBox1)
+        Me.Controls.Add(Me.inputHoursLabel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.YachtSizeLabel1)
+        Me.Controls.Add(Me.yachtSizeListBox1)
+        Me.Controls.Add(Me.yachtLabel)
+        Me.Controls.Add(Me.CalcButton)
+        Me.Controls.Add(Me.YachtsComboBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Yacht Report"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintSummaryToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintPreviewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintYachtTypesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintPreviewToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripTextBox1 As System.Windows.Forms.ToolStripTextBox
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintPreviewSummary As System.Windows.Forms.PrintPreviewDialog
+    Friend WithEvents PrintYachtsControl As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintSummaryControl As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintPreviewYacht As System.Windows.Forms.PrintPreviewDialog
+    Friend WithEvents YachtsComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents CalcButton As System.Windows.Forms.Button
+    Friend WithEvents yachtLabel As System.Windows.Forms.Label
+    Friend WithEvents yachtSizeListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents YachtSizeLabel1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents inputHoursLabel1 As System.Windows.Forms.Label
+    Friend WithEvents HoursTextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ VisualBasics/BrysonWatkinsAssignment7/BrysonWatkinsAssignment7/Form1.vb
@@ -0,0 +1,229 @@
+﻿Public Class Form1
+
+    Private TotalNumberCharters As Integer
+    Private TotalRvenueDecimal As Decimal
+    Private TotalHoursDec As Decimal
+    Private YachtSizeRate As Decimal
+
+
+    Private Sub PrintSummaryToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles PrintSummaryToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles HelpToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("Programmed by: Bryson Watkins", "About", MessageBoxButtons.OK)
+    End Sub
+
+    Private Sub PrintToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles PrintToolStripMenuItem.Click
+        'Print Summary
+        PrintSummaryControl.Print()
+
+    End Sub
+
+    Private Sub PrintPreviewToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles PrintPreviewToolStripMenuItem.Click
+        'Print Summary Preview
+        PrintPreviewSummary.Document = PrintSummaryControl
+        PrintPreviewSummary.ShowDialog()
+
+    End Sub
+
+    Private Sub PrintToolStripMenuItem1_Click(sender As System.Object, e As System.EventArgs) Handles PrintToolStripMenuItem1.Click
+        'Print Yacht Types
+        PrintYachtsControl.Print()
+
+
+    End Sub
+
+    Private Sub PrintPreviewToolStripMenuItem1_Click(sender As System.Object, e As System.EventArgs) Handles PrintPreviewToolStripMenuItem1.Click
+        'Print Yacht Preview
+        PrintPreviewYacht.Document = PrintYachtsControl
+        PrintPreviewYacht.ShowDialog()
+    End Sub
+
+    Private Sub YachtsComboBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles YachtsComboBox1.SelectedIndexChanged
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub PrintSummaryControl_PrintPage(sender As System.Object, e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintSummaryControl.PrintPage
+        Dim xposSingle As Single = e.MarginBounds.Left
+        Dim yposSingle As Single = e.MarginBounds.Top
+        Dim HeadingFont As New Font("Arial", 14, FontStyle.Bold)
+        Dim PrintingFont As New Font("Arial", 12)
+        Dim lineHeightSingle As Single = PrintingFont.GetHeight + 2
+        Dim PrintLineString As String
+        Dim PrintNameString As String
+        Dim avgHoursCharteredDec As Decimal
+        Dim StringSize As New SizeF()
+        Dim startpos As Single 'Start of the number column
+        Dim endpos As Single = 500.0F 'end of the number column
+        Dim FormattedNumString As String
+
+        'Calculations
+        'avgHoursCharteredDec = TotalHoursDec / TotalNumberCharters
+
+
+
+        PrintLineString = "Summary Report"
+        e.Graphics.DrawString(PrintLineString, HeadingFont, Brushes.Black, xposSingle, yposSingle)
+
+        yposSingle += lineHeightSingle
+        PrintNameString = "Bryson Watkins"
+        e.Graphics.DrawString(PrintNameString, HeadingFont, Brushes.Black, xposSingle, yposSingle)
+        yposSingle += lineHeightSingle * 2
+
+
+        PrintLineString = "Number of Charters"
+        e.Graphics.DrawString(PrintLineString, PrintingFont, Brushes.Black, xposSingle, yposSingle)
+        'print the number value
+        FormattedNumString = TotalNumberCharters.ToString("N0")
+
+        'calc start position
+        StringSize = e.Graphics.MeasureString(FormattedNumString, PrintingFont)
+        TotalNumberCharters = 1
+        startpos = endpos - StringSize.Width
+        e.Graphics.DrawString(FormattedNumString, PrintingFont, Brushes.Black, startpos, yposSingle)
+
+        yposSingle += lineHeightSingle 'line break
+
+        PrintLineString = "Total Revenue"
+        e.Graphics.DrawString(PrintLineString, PrintingFont, Brushes.Black, xposSingle, yposSingle)
+
+        yposSingle += lineHeightSingle 'line break
+
+        PrintLineString = "Average Hours Charted"
+        e.Graphics.DrawString(PrintLineString, PrintingFont, Brushes.Black, xposSingle, yposSingle)
+
+
+
+    End Sub
+
+    Private Sub CalcButton_Click(sender As System.Object, e As System.EventArgs) Handles CalcButton.Click
+        Dim goodDataBoolean As Boolean
+        Dim hourschartered As Decimal
+
+
+
+        'Validate Inputs
+
+        If YachtsComboBox1.Text = "" Then
+            MessageBox.Show("Please enter a yacht type", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            YachtsComboBox1.Focus()
+            YachtsComboBox1.SelectAll()
+            goodDataBoolean = False
+
+        ElseIf yachtSizeListBox1.SelectedIndex <= 0 Or yachtSizeListBox1.SelectedIndex > 7 Then
+            MessageBox.Show("Please select a valid yacht length.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            yachtSizeListBox1.Focus()
+            goodDataBoolean = False
+
+
+        ElseIf HoursTextBox1.Text = "" Then
+            MessageBox.Show("Please enter a positive whole number.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+
+            Try
+                hourschartered = Decimal.Parse(HoursTextBox1.Text)
+                If hourschartered <= 0 Then
+                    MessageBox.Show("Please enter a positive whole number of hours.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    HoursTextBox1.Focus()
+                    HoursTextBox1.SelectAll()
+                    goodDataBoolean = False
+                End If
+
+            Catch quantityErr As FormatException
+                MessageBox.Show("Quantity must be a positive whole number.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+
+
+
+        End If
+
+
+
+        'Calculate good data (find hourly rate, multiply hourly rate by hours chartered)
+
+
+        'display Calculations
+
+
+        'Accumulate totals
+
+
+    End Sub
+
+    Private Sub yachtLabel_Click(sender As System.Object, e As System.EventArgs) Handles yachtLabel.Click
+
+    End Sub
+
+    Private Sub AddYachtButton1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim ItemFoundBoolean As Boolean
+        Dim itemIndexInt As Integer
+
+        With YachtsComboBox1
+            If .Text = "" Then
+                MessageBox.Show("Please enter a yacht type.", "Data Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                Do Until ItemFoundBoolean Or itemIndexInt = .Items.Count
+                    'find for duplicates
+                    If .Text = .Items(itemIndexInt.ToString.ToUpper) Then
+                        ItemFoundBoolean = True
+                        Exit Do
+                    Else
+                        itemIndexInt += 1
+                    End If
+                Loop
+                If ItemFoundBoolean Then
+                    MessageBox.Show("Can't enter duplicate items", "Data Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+                Else
+                    .Items.Add(.Text)
+                End If
+
+            End If
+        End With
+
+
+    End Sub
+
+    Private Sub HoursTextBox1_TextChanged(sender As System.Object, e As System.EventArgs)
+        TotalHoursDec += Decimal.Parse(HoursTextBox1.Text)
+
+    End Sub
+
+    Private Sub yachtSizeListBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles yachtSizeListBox1.SelectedIndexChanged
+        Dim itemSelectedindexint As Integer
+        Dim itemCountint As Integer
+
+
+        itemSelectedindexint = yachtSizeListBox1.SelectedIndex + 1
+        itemCountint = yachtSizeListBox1.Items.Count
+
+        If itemSelectedindexint = 1 Then
+            YachtSizeRate = 95.0
+        ElseIf itemSelectedindexint = 2 Then
+            YachtSizeRate = 137.0
+        ElseIf itemSelectedindexint = 3 Then
+            YachtSizeRate = 160.0
+        ElseIf itemSelectedindexint = 4 Then
+            YachtSizeRate = 192.0
+        ElseIf itemSelectedindexint = 5 Then
+            YachtSizeRate = 250.0
+        ElseIf itemSelectedindexint = 6 Then
+            YachtSizeRate = 400.0
+        ElseIf itemSelectedindexint = 7 Then
+            YachtSizeRate = 550.0
+        End If
+
+
+    End Sub
+End Class
